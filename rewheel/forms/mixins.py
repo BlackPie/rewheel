5e2c71a4c913f53de3c6427788ac2193babfb75f
Dmitry Fedotkin <fedotkin.dmitry@gmail.com>
2016-02-22 00:43:31 +0500

    added EqualFieldsFormMixin

--- rewheel/forms/mixins.py
+++ rewheel/forms/mixins.py
@@ -6,3 +6,16 @@ class UserReceivingFormMixin(object):
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop("user", None)
         super(UserReceivingFormMixin, self).__init__(*args, **kwargs)
+
+
+class EqualFieldsFormMixin(object):
+    """
+    This mixin gets all fields names mentioned in self.equal_fields
+    and check these fields data equality.
+    """
+    equal_fields = []
+
+    def clean(self):
+        cleaned_data = super(EqualFieldsFormMixin, self).clean()
+        fields_data = [cleaned_data[x] for x in self.equal_fields]
+        return len(set(fields_data)) <= 1
